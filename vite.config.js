5bbc5e27deca49a0e4843c54a805bac0904671b2
Roger Jungemann <rtaljun@gmail.com>
2024-08-29 12:23:25 -0700

    Prepare for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
 import path from "path"
 import { fileURLToPath } from 'url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "src": "/src",
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname);
+  const nodeEnv = env.NODE_ENV ?? 'development'
+  const isDevelopment = nodeEnv === 'development'
+  return {
+    ...(isDevelopment ? {} : { base: '/chiaro-mini/' }),
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "src": "/src",
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
